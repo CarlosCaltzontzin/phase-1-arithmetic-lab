ca4da35572b2e4701503044fc8f524fef4074e62
Carlos Caltzontzin <cacr71993@yahoo.com>
2023-04-25 09:29:45 -0500

    Completed phase 1 arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+// Write your code here 
+const num1 = 2;
+const num2 = 31;
+const multiply =  num1 * num2;
+
+const random = Math.ceil(Math.random() + 1);
+
+const num3 = 22;
+const num4 = 6;
+const mod = num3 % num4;
+
+const max = Math.max(1, 3, 6, 8, 13, 18, 20);
+
